fade2ba2c00aea9c0ce27f3fd77df36134ef2be5
samleemcknight <samleemcknight@gmail.com>
2021-02-23 16:01:15 -0700

    makes changes to seed files to avoid duplication in database and adds a dbTest.js file

--- seeders/20210223180241-sampleUsers.js
+++ seeders/20210223180241-sampleUsers.js
@@ -1,23 +1,30 @@
 'use strict';
+const bcrypt = require('bcrypt')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [
+    // clears db so there won't be duplicates after seed
+    await queryInterface.bulkDelete('users', null, {truncate: true,
+      cascade: true, restartIdentity: true
+    });
+
+    const bulkUsers = await queryInterface.bulkInsert('users', [
     {
       name: "User1",
       email: "user1@user.com",
-      password: "password",
+      password: bcrypt.hashSync("password", 12),
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       name: "User2",
       email: "user2@user.com",
-      password: "password",
+      password: bcrypt.hashSync("password", 12),
       createdAt: new Date(),
       updatedAt: new Date()
     },
-  ])
+  ], {returning: true});
+    console.log("users: ", bulkUsers)
   },
 
   down: async (queryInterface, Sequelize) => {
