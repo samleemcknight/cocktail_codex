09a726455459fb90cb869fd03ff2fb342fcebe1f
mh610creative <nonsensemh1@gmail.com>
2021-02-23 14:48:30 -0800

    revisions to routes, added views, discussed potential problem of renders,redirects

--- routes/cocktails.js
+++ routes/cocktails.js
@@ -17,45 +17,45 @@ router.get('/cocktails/:id', isLoggedIn, (req, res) => {
 
 // POST ROUTE - when user adds to my favorites from 1Cocktail Get Page
 router.post('/cocktails/:id', isLoggedIn, (req, res) => {
-    res.redirect('favorites');
+    res.redirect('/cocktails');
 });
 
 // GET ROUTE - users favorites
-router.get('/cocktails/myCocktails', isLoggedIn, (req, res) => {
+router.get('/myCocktails', isLoggedIn, (req, res) => {
     res.render('favorites');
 });
 
 // DELETE Route - Deletes one cocktail from users favorites, delete from individual page or main listing of favorites?
-router.delete('/cocktails/myCocktails/:id', (req, res) => {
-    res.redirect('favorites');
+router.delete('/myCocktails/:id', (req, res) => {
+    res.redirect('/myCocktails');
 });
 
 // GET Route- Sam's edit recipe ideas functionality, SHOWS ONE to edit from favorites get route
-router.get('/cocktails/myCocktails/:id', isLoggedIn, (req, res) => {
+router.get('/myCocktails/:id', isLoggedIn, (req, res) => {
     res.render('myOneCocktail');
 });
 
 // PUT ROUTE- Sam's edit recipe idea, EDITS ONE that was shown
-router.put('/cocktails/myCocktails/edit/:id', isLoggedIn, (req, res) => {
-    res.redirect('myOneCocktail');
+router.put('/myCocktails/edit/:id', isLoggedIn, (req, res) => {
+    res.redirect('/myCocktails');
 });
 
 // BONUS
 
 // SEARCH COCKTAILS BY PRIMARY
 // GET ROUTE FOR SEARCH
-router.get('/cocktails/prime', isLoggedIn, (req, res) => {
+router.get('/prime', isLoggedIn, (req, res) => {
     res.render('searchPrime');
 });
 
 // GET ROUTE FOR ONE DRINK SEARCHED
-router.get('/cocktails/prime/:id', isLoggedIn, (req, res) => {
+router.get('/prime/:id', isLoggedIn, (req, res) => {
     res.render('prime');
 });
 
 // POST ROUTE - when user adds to my favorites from 1Cocktail primary alcohol Get Page
-router.post('/cocktails/prime/:id', isLoggedIn, (req, res) => {
-    res.redirect('favorites');
+router.post('/prime/:id', isLoggedIn, (req, res) => {
+    res.redirect('/myCocktails');
 });
 
 
