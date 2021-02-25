be51583584680244f1192efdb8cd9d7c1b258d59
samleemcknight <samleemcknight@gmail.com>
2021-02-24 17:19:29 -0700

    adds post route for favorite cocktails

--- routes/cocktails.js
+++ routes/cocktails.js
@@ -3,17 +3,53 @@ const router = express.Router();
 const passport = require('../config/ppConfig')
 const db = require("../models")
 const isLoggedIn = require('../middleware/isLoggedIn')
+const { Op } = require('sequelize')
 
 // ROUTERS COCKTAILS
 
-// GET ROUTE FOR SEARCH
+// GET ROUTE FOR SEARCH PAGE
+
 router.get('/', isLoggedIn, (req, res) => {
-    db.cocktail.findAll()
-    .then(cocktails => {
-        console.log(cocktails)
-        res.render('cocktails/search', {cocktails: cocktails})
-    })
+    let cocktails = null
+    res.render('cocktails/search', {cocktails: cocktails})
 });
+
+let searchedCocktails = null
+// POST route for search function
+router.post('/search', (req, res) => {
+    db.cocktail.findAll({
+        where: {
+            [Op.or]: [
+                { name: req.body.searchTerm },
+                { primaryAlcohol: req.body.searchTerm },
+            ]
+        }
+    }).then(cocktails => {
+        searchedCocktails = cocktails
+        res.render('cocktails/search', { cocktails: cocktails })
+    })
+})
+
+// POST for add to favorites
+router.post('/myFavorites', (req, res) => {
+    
+    db.user.findOne({
+        where: { email: req.user.dataValues.email}
+    }).then(user => {
+        
+        db.cocktail.findOne({
+            where: { name: req.body.name}
+        }).then(cocktail => {
+            // need to add flash message showing that cocktails were added to db
+            // additionally, in the return searched cocktails, take out the cocktail that was added
+            // or somehow render the cocktails in such a way that the added cocktail doesn't appear again
+            user.addCocktail(cocktail).then(relationInfo => {
+                req.flash('success', 'successfully added')
+                res.redirect('/cocktails')
+            })
+        })
+    })
+})
 
 // GET ROUTE - list info about specific drink with add to myCocktails functionality 
 router.get('/:id', isLoggedIn, (req, res) => {
@@ -21,9 +57,9 @@ router.get('/:id', isLoggedIn, (req, res) => {
 });
 
 // POST ROUTE - when user adds to my favorites from 1Cocktail Get Page
-router.post('/:id', isLoggedIn, (req, res) => {
-    res.redirect('/');
-});
+// router.post('/:id', isLoggedIn, (req, res) => {
+//     res.redirect('/');
+// });
 
 // GET ROUTE - users favorites
 router.get('/myCocktails', isLoggedIn, (req, res) => {
